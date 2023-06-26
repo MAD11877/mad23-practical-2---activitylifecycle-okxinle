b155eddeaa34fc2dd62fb340ab71636f1ee6400a
Xin Le Chong <s10221973@connect.np.edu.sg>
2023-06-26 09:55:32 +0800

    try

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -1,14 +1,13 @@
 package sg.edu.np.mad.madpractical;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String TITLE = "Main Activity";
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,24 +15,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create a User object and load name and description values into their respective views
-        User user = new User();
+        user = new User();
         user.setFollowed(false);
 
-        // set text base on value of the followed variable
+        // Set the initial button text based on the followed variable
         Button followButton = findViewById(R.id.button);
+        updateFollowButton();
 
         // Set an OnClickListener to toggle the button text and update the followed variable
         followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (user.isFollowed() == false) {
-                    followButton.setText("Unfollow");
-                    user.setFollowed(true);
-                } else {
-                    followButton.setText("Follow");
-                    user.setFollowed(false);
-                }
+                user.setFollowed(!user.isFollowed());
+                updateFollowButton();
             }
         });
+    }
+
+    private void updateFollowButton() {
+        Button followButton = findViewById(R.id.button);
+        if (user.isFollowed()) {
+            followButton.setText("Unfollow");
+        } else {
+            followButton.setText("Follow");
+        }
     }
 }
